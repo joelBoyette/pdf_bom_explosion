8a6ad0d94674e96406706bc4d35d331d40ea89b0
Joel Boyette <jboyette@brlee.local>
2020-02-28 16:35:00 -0500

    added pyinstaller for distribution, working on gui

--- testing.py
+++ testing.py
@@ -1,43 +1,39 @@
 import pandas as pd
 import re
 import numpy as np
+from tkinter import *
+# user_top_level = input("Please Enter Top Level")
+# user_qty = input("Please enter a qty")
+#
+# print(f'user top level {user_top_level} and qty {user_qty}')
 
-string = r'\1021045\1021338\1016217'
 
-bom_explosion_df = \
-    pd.read_excel(r'C:\Users\JBoyette.BRLEE\Documents\Development\test_data\pdf_bom_explosion\1021747_02-26-2020.xlsx')
 
-bom_explosion_df['Top Level'] = bom_explosion_df['Sort Path'][0][1:]
-bom_explosion_df['BOM Details [BOM Path | Parent Q/P | Comp Q/P]'] = bom_explosion_df[['Sort Path', 'Assembly Q/P',
-                                                                                       'Comp Q/P']].astype(str) \
-                                                    .apply(lambda x: '[' + '-'.join(x) + ']' + '\n'
-                                                            if x.all != '' else set(x), axis=1)
 
-bom_explosion_df = bom_explosion_df.drop(['Assembly', 'Assembly Make Qty', 'Comp Extd Qty',
-                                          'Sort Path', 'Level', 'Comp Q/P', 'Assembly Q/P'], axis=1)
 
-bom_explosion_df = bom_explosion_df.rename(columns={'PartDescription': 'Description',
-                                                    'TypeCode': 'Type'})
+def main():
 
-bom_explosion_df = bom_explosion_df[['Part', 'Description', 'Type', 'Dwg Link', 'Total Needed', 'Supply Status',
-                                     'Main Bins', 'Floor Bins', 'OH', 'OH_Inspect', 'ONO', 'First Due', 'DMD', 'Buyer',
-                                     'Cost', 'Top Level', '# Top Level to Make',
-                                     'BOM Details [BOM Path | Parent Q/P | Comp Q/P]']]
-bom_explosion_df = bom_explosion_df.astype(str)
+    window = Tk()
+    window.geometry('350x200')
 
-# https://thispointer.com/python-how-to-use-if-else-elif-in-lambda-functions/
-pivot_f = lambda x: x.sum() if np.issubdtype(x.dtype, np.number) \
-                            else (''.join(list(set(x)))
-                                  if x.name in ['BOM Details [BOM Path | Parent Q/P | Comp Q/P]']
-                                  else list(set(x))[0])
+    top_level_lbl = Label(window, text='Top Level', padx=20, pady=10).grid(column=0, row=0)
+    top_level_txtbx = Entry(window, width=10)
+    top_level_txtbx.grid(column=0, row=1, padx=1, pady=1)
 
-cols = bom_explosion_df.columns[~bom_explosion_df.columns.isin(['Part'])].tolist()
-pivtbl = pd.pivot_table(bom_explosion_df,
-                        index=['Part'],
-                        values=cols,
-                        aggfunc=pivot_f,
-                        fill_value='').reindex(columns=cols).reset_index()
+    qty_lbl = Label(window, text='Make Qty', padx=20, pady=10).grid(column=0, row=2)
+    qty_txtbx = Entry(window, width=10)
+    qty_txtbx.grid(column=0, row=3, padx=1, pady=1)
 
-print(pivtbl.head(5))
+    def explode_assembly():
+        top_level_input = top_level_txtbx.get()
+        qty_input = qty_txtbx.get()
+        print(f'part {top_level_input} and qty {qty_input}')
 
-pivtbl.to_excel(r'C:\Users\JBoyette.BRLEE\Documents\Development\test_data\pdf_bom_explosion\test.xlsx')
+    btn = Button(window, text="Explode BOM", padx=1, pady=1, command=explode_assembly)
+    btn.grid(column=0, row=5)
+
+    window.mainloop()
+
+
+if __name__ == '__main__':
+    main()
